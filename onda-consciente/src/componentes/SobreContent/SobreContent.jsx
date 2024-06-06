7992cf2679eda5a2ce81794cb3970006fa902167
Giovanna Franco <rm553701@fiap.com.br>
2024-06-06 19:53:20 -0300

    removendo coisas desnecessária

--- onda-consciente/src/componentes/SobreContent/SobreContent.jsx
+++ onda-consciente/src/componentes/SobreContent/SobreContent.jsx
@@ -20,19 +20,12 @@ function SobreContent() {
                         <div className='sobre-item'>
                             <img src="https://img.odcdn.com.br/wp-content/uploads/2023/06/nivel-do-mar.jpg" alt="Foto nível do mar" />
                         </div>
-                        {/* <div>
-                            <p>A implementação desse sistema é de extrema importância para diversas áreas, especialmente nos estudos ambientais e na emissão de alertas de enchentes. Através de medições contínuas e precisas, pesquisadores e autoridades podem acompanhar as variações no nível do mar, permitindo a análise de dados históricos e a previsão de eventos críticos. Este tipo de monitoramento é crucial para a implementação de medidas preventivas e mitigação de desastres naturais, além de contribuir para o entendimento dos impactos das mudanças climáticas.</p>
-                        </div> */}
+                        
                         <div >
                             <p>Em resumo, este projeto representa uma solução tecnológica inovadora para o monitoramento do nível do mar, oferecendo uma ferramenta valiosa para a preservação ambiental e a segurança das comunidades costeiras. A capacidade de medir e reportar variações no nível do mar de forma precisa e em tempo real pode fazer a diferença na resposta a eventos climáticos extremos e na gestão de recursos hídricos.</p>
                             <a href="https://wokwi.com/projects/399682901295263745"> Simulador do Monitoramento em Tempo Real do Nível do Mar</a>
                         </div>
-                    </div>
-                    {/* <div className='item-sobre'> 
-                        <img src="https://img.odcdn.com.br/wp-content/uploads/2023/06/nivel-do-mar.jpg" alt="Foto nível do mar" />
-                        <p>Em resumo, este projeto representa uma solução tecnológica inovadora para o monitoramento do nível do mar, oferecendo uma ferramenta valiosa para a preservação ambiental e a segurança das comunidades costeiras. A capacidade de medir e reportar variações no nível do mar de forma precisa e em tempo real pode fazer a diferença na resposta a eventos climáticos extremos e na gestão de recursos hídricos.</p>
-                        <a href="https://wokwi.com/projects/399682901295263745"> Simulador do Monitoramento em Tempo Real do Nível do Mar</a>
-                    </div> */}
+                    </div>                    
                     {/* <div className='circuito' >
                         <img src={circuito} alt="Foto Circuito" />
                     </div> */}
